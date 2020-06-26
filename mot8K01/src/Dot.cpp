d95e528e9fe5df488544099b9c194ff054c2d809
Atsushi Tadokoro <tadokoro@gmail.com>
2020-06-26 23:39:28 +0900

    Update Dot.cpp

--- mot8K01/src/Dot.cpp
+++ mot8K01/src/Dot.cpp
@@ -7,7 +7,7 @@ Dot::Dot(glm::vec2 _center) {
 }
 
 void Dot::update() {
-	float div = 120.0 / ofGetElapsedTimef() + 0.5;
+	float div = 120.0 / ofGetElapsedTimef() + 1.0;
 	float speed = 60.0 / ofGetElapsedTimef() + 0.5;
 	float shiftX = -ofGetElapsedTimef() * 0.2;
 	float shiftY = ofGetElapsedTimef() * 0.5;
@@ -15,7 +15,7 @@ void Dot::update() {
 	color.g = ofNoise(center.x / ofGetWidth() * div + shiftX, center.y / ofGetWidth() * div + shiftY, ofGetElapsedTimef() * speed + 0.12) * 255;
 	color.b = ofNoise(center.x / ofGetWidth() * div + shiftX, center.y / ofGetWidth() * div + shiftY, ofGetElapsedTimef() * speed + 0.24) * 255;
 
-	globalLevel += 1.0 / 240.0;
+	globalLevel += 1.0 / 600.0;
 	if (globalLevel > 1.0) {
 		globalLevel = 1.0;
 	}
@@ -27,25 +27,26 @@ void Dot::update() {
 
 void Dot::draw() {
 	int baseColor = 0;
-	float r = ofGetWidth() / 800.0;
+	float r = ofGetWidth() / 400.0;
 	float shift = -r / 2.5;
 	ofSetCircleResolution(64);
 	ofEnableBlendMode(OF_BLENDMODE_ADD);
 	ofPushMatrix();
 	float shiftLoc = color.getBrightness() / 255.0 * 6.0 * shiftLevel;
 	ofTranslate(center.x + shiftLoc, center.y);
+	ofScale(globalLevel, globalLevel);
 	ofSetColor(255, baseColor, baseColor);
-	float rrad = ofMap(color.r, 0, 255, 0, r * globalLevel);
+	float rrad = ofMap(color.r, 0, 255, 0, r);
 	ofDrawCircle(0, shift, rrad);
 
 	ofRotateDeg(120);
 	ofSetColor(baseColor, 255, baseColor);
-	float grad = ofMap(color.g, 0, 255, 0, r * globalLevel);
+	float grad = ofMap(color.g, 0, 255, 0, r);
 	ofDrawCircle(0, shift, grad);
 
 	ofRotateDeg(120);
 	ofSetColor(baseColor, baseColor, 255);
-	float brad = ofMap(color.b, 0, 255, 0, r * globalLevel);
+	float brad = ofMap(color.b, 0, 255, 0, r);
 	ofDrawCircle(0, shift, brad);
 	ofPopMatrix();
 }
